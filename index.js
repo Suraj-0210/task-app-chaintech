2ab053103c7b3422e3322c43a2a969136c334f22
Suryakanta Prusty <kantaprustys@gmail.com>
2024-12-26 13:39:48 +0530

    updated the url

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 const __diname = path.resolve();
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Your frontend URL
+  origin: "http://localhost:5173, https://task-app-8fjy.onrender.com", // Your frontend URL
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // This allows cookies to be sent with requests
