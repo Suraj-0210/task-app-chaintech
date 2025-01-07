5a5e3f09b8a2bf930a8bb31e969f4abe3a8a5d89
Suryakanta Prusty <kantaprustys@gmail.com>
2025-01-07 10:43:45 +0530

    deploy-8

--- utils/generateJWTcookie.js
+++ utils/generateJWTcookie.js
@@ -2,15 +2,17 @@ import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "15d",
+    expiresIn: "15d", // Token expiration
   });
 
   res.cookie("jwt", token, {
-    maxAge: 15 * 14 * 60 * 60 * 1000,
-    httpOnly: false,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "Local",
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
+    httpOnly: true, // Prevent access to the cookie via client-side JS
+    sameSite: process.env.NODE_ENV === "Prod" ? "strict" : "lax", // Use lax in development
+    secure: process.env.NODE_ENV === "Prod", // Use secure cookies in production
   });
+
+  console.log("Token and cookie successfully generated");
 };
 
 export default generateTokenAndSetCookie;
